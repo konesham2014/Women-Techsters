cb0d5dcfce3a7e88799940351c3f1efd9b170ca4
Senait Mekonen Gebreegziabher <konesham1993@gmail.com>
2025-09-04 16:15:20 +0300

    Weekly exam

---
+++ women-techsters-fellowship/new.js
@@ -0,0 +1,17 @@
+let num1 = 12
+let num2 = 4
+let name = "Senait"
+
+let addition = num1 + num2
+console.log("Addition:", addition)
+
+let subtraction = num1 - num2
+console.log("Subtraction:", subtraction)
+
+let multiplication = num1 * num2
+console.log("Multiplication:", multiplication)
+
+let division = num1 / num2
+console.log("Division:", division)
+
+console.log(`Hello ${name}, the result of num1 + num2 is ${addition}`)
