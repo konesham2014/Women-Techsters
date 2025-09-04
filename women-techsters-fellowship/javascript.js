b347d868b228aa66d222becefe62a3e7492505e5
Senait Mekonen Gebreegziabher <konesham1993@gmail.com>
2025-09-04 15:38:07 +0300

    My first website

---
+++ women-techsters-fellowship/javascript.js
@@ -0,0 +1,6 @@
+const amHuman = confirm("Are you human?") // confirm gives you a yes or no reply.
+const name = prompt("what is your name?")
+console.log(name)
+
+let jobTitle = prompt("what is your job role?")
+console.log(jobTitle)
